a7401977f3f34a06f619d299973c2b1a940bb746
woonsik <dnstlr2933@naver.com>
2021-01-06 20:46:02 +0900

    구간합

--- 구간합구하기4_11659.java
+++ 구간합구하기4_11659.java
@@ -1,5 +1,3 @@
-package Gold이상문제_정리;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- 연결요소의개수_11724.java
+++ 연결요소의개수_11724.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class 연결요소의개수_11724 {
+import java.util.*;
+import java.io.*;
+
+public class 연결요소의개수_11724 {
+    static boolean[] visited;
+    static int[][] map;
+    static int N,M;
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        map = new int[N + 1][N + 1];
+
+        for (int i = 0; i < M; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            map[u][v] = 1;
+            map[v][u] = 1;
+        }
+
+        int count = 0;
+        visited = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                count++;
+                check(i);
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static void check(int i) {
+        visited[i] = true;
+        for (int j = 1; j <= N; j++) {
+            if (map[i][j] == 1 && !visited[j])
+                check(j);
+        }
+    }
 }
