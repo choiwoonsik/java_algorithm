0eeeeb61e734981aab2f8cd3df11784adabfca9b
woonsik <dnstlr2933@naver.com>
2021-01-16 13:47:59 +0900

    연습문제용-Main함수

---
+++ Gold이상문제_정리/Main.java
@@ -0,0 +1,10 @@
+package Gold이상문제_정리;
+
+public class Main {
+	public static void main(String[] args) {
+		Integer a = 10, b = a+a, c = 20;
+		Integer d = 50000, e = d+d, f = 100000;
+		System.out.printf("b == c : %s%n", b == c);
+		System.out.printf("e == f : %s%n", e == f);
+	}
+}
