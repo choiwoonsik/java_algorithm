0fdabd42c169b7c0f1ce624d62a2cc1eceab2bda
woonsik <dnstlr2933@naver.com>
2021-02-10 13:57:52 +0900

    백트래킹 - 백트래킹으로 모든 경우의 수를 확인하면서 조건이 충족되면 출력 문자열에 추가하는 방식으로 푼다, 종료조건은 문자열의 길이가 충족된 경우 return한다

---
+++ Gold이상문제_정리/좋은수열2_2661.java
@@ -0,0 +1,46 @@
+package Gold이상문제_정리;
+
+import java.io.*;
+import java.util.*;
+
+public class 좋은수열2_2661 {
+	static StringBuilder str = new StringBuilder();
+	static int N;
+	static boolean isDone;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+
+		str.append("1");
+		backtracking(str);
+	}
+
+	private static void backtracking(StringBuilder s) {
+		if (s.length() == N && !isDone) {
+			System.out.println(s);
+			isDone = true;
+			return;
+		}
+		if (isDone) return;
+
+		for (int i = 1; i <= 3; i++) {
+			if (isPossible(i, str)) {
+				str.append(i);
+				backtracking(s);
+				str.delete(str.length()-1, str.length());
+			}
+		}
+	}
+
+	private static boolean isPossible(int num, StringBuilder s) {
+		s.append(num);
+		for (int i = 1; i <= s.length()/2; i++) {
+			if (s.substring(s.length() - i, s.length()).equals(s.substring(s.length() - 2 * i, s.length() - i))) {
+				s.delete(s.length()-1, s.length());
+				return false;
+			}
+		}
+		s.delete(s.length()-1, s.length());
+		return true;
+	}
+}
